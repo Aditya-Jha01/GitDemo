07d0cda331b9f906bb1675c32f0027b1a43ff0e7
aditya <koaditya44@gmail.com>
2020-09-08 09:48:58 +0530

    this is commit from Asian Timezone guy

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -7,7 +7,9 @@ public class RESTAPITest {
 	@Test
 	public void postJira()
 	{
-		System.out.println("Post Jira");
+		System.out.println("Post Jira 1");
+		System.out.println("Post Jira 2");
+		System.out.println("Post Jira 3");
 	}
 	
 	@Test
